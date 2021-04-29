c73ac48421fa84ef991c2f67fe24b49a26676880
Rovshan Jafarov <rovshan.j@agz.local>
2021-04-29 17:33:11 +0400

    Main section continue.Welcome section

---
+++ js/index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".mySlider").owlCarousel({
+    loop: false,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+        dots: true,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+  
+  $(".carousel-item").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+});
